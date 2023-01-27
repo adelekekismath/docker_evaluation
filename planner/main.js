f562839d466a494a77ee210bf7f938285a2a8a0a
adelekekismath <adelekekismath9@gmail.com>
2023-01-27 17:37:38 +0100

    exo 4

--- planner/main.js
+++ planner/main.js
@@ -1,54 +1,54 @@
-require('dotenv').config()
-const fetch = require('node-fetch')
-const express = require('express')
+require("dotenv").config();
+const fetch = require("node-fetch");
+const express = require("express");
 
-const port = process.env.PORT || 3000
-const nbTasks = parseInt(process.env.TASKS) || 20
+const port = process.env.PORT || 3000;
+const nbTasks = parseInt(process.env.TASKS) || 20;
 
-const randInt = (min, max) => Math.floor(Math.random() * (max - min)) + min
-const taskType = () => (randInt(0, 2) ? 'mult' : 'add')
-const args = () => ({ a: randInt(0, 40), b: randInt(0, 40) })
+const randInt = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+const taskType = () => (randInt(0, 2) ? "mult" : "add");
+const args = () => ({ a: randInt(0, 40), b: randInt(0, 40) });
 
 const generateTasks = (i) =>
-  new Array(i).fill(1).map((_) => ({ type: taskType(), args: args() }))
+  new Array(i).fill(1).map((_) => ({ type: taskType(), args: args() }));
 
 let workers = [
-  { url: "http://worker:8080", id: "0", type: "add" },
+  /*{ url: "http://worker:8080", id: "0", type: "add" },
   { url: "http://worker1:8080", id: "1", type: "mult" },
   { url: "http://worker2:8080", id: "2", type: "add" },
-  { url: "http://worker3:8080", id: "3", type: "mult" },
+  { url: "http://worker3:8080", id: "3", type: "mult" },*/
 ];
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 app.use(
   express.urlencoded({
     extended: true,
   })
-)
+);
 
-app.get('/', (req, res) => {
-  res.send(JSON.stringify(workers))
-})
+app.get("/", (req, res) => {
+  res.send(JSON.stringify(workers));
+});
 
-app.post('/register', (req, res) => {
-  const { url, id } = req.body
-  console.log(`Register: adding ${url} worker: ${id}`)
-  workers.push({ url, id })
-  res.send('ok')
-})
+app.post("/register", (req, res) => {
+  const { url: url, id: id, type: type } = req.body;
+  console.log(`Register: adding ${url} worker: ${id}`);
+  workers.push({ url: url, id: id, type: type });
+  res.send("ok");
+});
 
-let tasks = generateTasks(nbTasks)
-let taskToDo = nbTasks
+let tasks = generateTasks(nbTasks);
+let taskToDo = nbTasks;
 
 const wait = (mili) =>
-  new Promise((resolve, reject) => setTimeout(resolve, mili))
+  new Promise((resolve, reject) => setTimeout(resolve, mili));
 
 const sendTask = async (worker, task) => {
-  console.log(`=> ${worker.url}/${task.type}`, task)
-  workers = workers.filter((w) => w.id !== worker.id)
-  tasks = tasks.filter((t) => t !== task)
-  if (worker.type == tasks || worker.type== "") {
+  console.log(`=> ${worker.url}/${task.type}`, task);
+  workers = workers.filter((w) => w.id !== worker.id);
+  tasks = tasks.filter((t) => t !== task);
+  if (worker.type == task || worker.type == "") {
     const request = fetch(`${worker.url}/${task.type}`, {
       method: "POST",
       headers: {
@@ -69,29 +69,26 @@ const sendTask = async (worker, task) => {
         console.log("---");
         return res;
       });
-  }
-  else {
+  } else {
     tasks = [...tasks, task];
-    workers = [...workers, worker];
-    sendTask(worker[0],task);
+    if(!worker.length==0)
+      workers = [...workers, worker];
   }
-  
-   
-}
+};
 
 const main = async () => {
-  console.log(tasks)
+  console.log(tasks);
   while (taskToDo > 0) {
-    await wait(100)
-    if (workers.length === 0 || tasks.length === 0) continue
-    sendTask(workers[0], tasks[0])
+    await wait(100);
+    if (workers.length === 0 || tasks.length === 0) continue;
+    sendTask(workers[0], tasks[0]);
   }
-  console.log('end of tasks')
-  server.close()
-}
+  console.log("end of tasks");
+  server.close();
+};
 
 const server = app.listen(port, () => {
-  console.log(`Register listening at http://localhost:${port}`)
-  console.log('starting tasks...')
-  main()
-})
+  console.log(`Register listening at http://localhost:${port}`);
+  console.log("starting tasks...");
+  main();
+});
--- run.sh
+++ run.sh
@@ -29,3 +29,13 @@ docker run --network=workplan -e PORT=8080 -e ADD=false -e PLANNER=http://planne
 #Apres le lancement des commandes ci -dessus j'ai remarqué que lorsque un worker n'a pas le meme type que la task
 #il renvoie une erreur de invalid json, du coup j'ai rajouté attribut type dans la table workers pour verifier 
 #le type avant de lui attribuer une tache
+
+#ajout de la variable ADDRESS
+
+docker run --network=workplan -e PORT=8080 -e MULT=false -e PLANNER=http://planner:3000 -e ADDRESS=http://worker:8080 --name worker -d  workplan/worker
+docker run --network=workplan -e PORT=8080 -e ADD=false -e PLANNER=http://planner:3000 -e ADDRESS=http://worker1:8080 --name  worker1 -d  workplan/worker
+docker run --network=workplan -e PORT=8080 -e MULT=false -e PLANNER=http://planner:3000 -e ADDRESS=http://worker2:8080  --name worker2 -d  workplan/worker
+docker run --network=workplan -e PORT=8080 -e ADD=false -e PLANNER=http://planner:3000 -e ADDRESS=http://worker3:8080 --name worker3 -d  workplan/worker
+
+#j'ai pas reussi a demarrer enregistrer dynamiquement les workers, je pense que c'est parce que j'ai changé le code 
+#qui gere les erreurs
